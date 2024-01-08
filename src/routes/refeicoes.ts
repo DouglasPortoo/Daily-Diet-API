8c004b9e40161bc3787f4b07f44757966d216dca
Douglas <portogdouglas@gmail.com>
2024-01-08 16:31:35 -0300

    registrando uma refeição no db

--- src/routes/refeicoes.ts
+++ src/routes/refeicoes.ts
@@ -1,3 +1,30 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { checkSessionIdExists } from "../../middlewares/check-session-id-exists";
+import { knex } from "../database";
+import { randomUUID } from "crypto";
 
-export async function mealsRoutes(app: FastifyInstance){}
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post("/", { preHandler: [checkSessionIdExists] }, async (req,replay) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isDiet: z.boolean(),
+    });
+
+    const { name, description, isDiet } = createMealBodySchema.parse(req.body);
+
+    const { sessionId } = req.cookies;
+    const user = await knex("users").where("session_id", sessionId).first();
+
+    await knex("meals").insert({
+      id: randomUUID(),
+      name,
+      description,
+      is_diet: isDiet,
+      user_id: user.id,
+    });
+
+    return replay.status(201).send();
+  });
+}
--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,40 +1,43 @@
 import { randomUUID } from "crypto";
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
-import crypto from "node:crypto"
+import crypto from "node:crypto";
 import { knex } from "../database";
 
-export async function userRoutes(app: FastifyInstance){
-
-  app.post("/", async(req,replay)=>{
+export async function userRoutes(app: FastifyInstance) {
+  app.post("/", async (req, replay) => {
     const createUserBodySchema = z.object({
-      name:z.string(),
-      email:z.string(),
-      password:z.string()
-    })
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
+    });
 
-    const {name,email,password} = createUserBodySchema.parse(req.body)
+    const { name, email, password } = createUserBodySchema.parse(req.body);
 
-    let sessionId = req.cookies.sessionId
+    const userExist = await knex("users").where("email", email).first();
 
-    if(!sessionId){
-      sessionId = randomUUID();
+    let sessionId = randomUUID();
 
-      replay.cookie("sessionId", sessionId, {
-        path: "/",
-        maxAge: 1000 * 60 * 30 // 30 minutos
+    replay.cookie("sessionId", sessionId, {
+      path: "/",
+      maxAge: 1000 * 60 * 30, // 30 minutos
+    });
+
+    if (!userExist) {
+      const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+      await knex("users").insert({
+        id: randomUUID(),
+        name,
+        email,
+        password: hash,
+        session_id: sessionId,
       });
+
+      return { name, email, password };
+    } else {
+      const newSessionId = (userExist.session_id = sessionId);
+      await knex("users").update("session_id", newSessionId);
     }
-
-    const hash = crypto.createHash('sha256').update(password).digest('hex');
-
-    await knex("users").insert({
-      id: randomUUID(),
-      name,
-      email,
-      password : hash
-    })
-
-    return ({name,email,password})
-  })
+  });
 }
